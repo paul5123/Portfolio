2df3929a6ac1caed8b8a2375b940737edcc3ee6c
Paul5123 <paul.guillon17@orange.fr>
2026-09-04 17:37:28 +0200

    Ajout du contenu du composant Skills

---
+++ src/components/Skills/index.jsx
@@ -0,0 +1,24 @@
+function Skills() {
+  return (
+    <section id="skills">
+      <h2>Mes compétences</h2>
+
+      <div>
+        <h3>Front-end</h3>
+        <p>HTML, CSS, JavaScript, React, React Router, Sass</p>
+      </div>
+
+      <div>
+        <h3>Back-end</h3>
+        <p>Node.js, Express, MongoDB, Mongoose</p>
+      </div>
+
+      <div>
+        <h3>Qualité web</h3>
+        <p>Responsive design, accessibilité, SEO, performance</p>
+      </div>
+    </section>
+  );
+}
+
+export default Skills;
