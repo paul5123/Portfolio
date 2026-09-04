c5ce1b8af3902645519219fc04cd906f1668a088
Paul5123 <paul.guillon17@orange.fr>
2026-09-04 17:37:14 +0200

    Ajout du contenu du composant projects

---
+++ src/components/Projects/index.jsx
@@ -0,0 +1,13 @@
+function Projects() {
+  return (
+    <section id="projects">
+      <h2>Mes projets</h2>
+
+      <p>
+        Découvrez une sélection des projets réalisés au cours de ma formation.
+      </p>
+    </section>
+  );
+}
+
+export default Projects;
