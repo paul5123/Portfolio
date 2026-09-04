3212c2ba39ed21c02d446da6ceeb135a9394c7cb
Paul5123 <paul.guillon17@orange.fr>
2026-09-04 17:45:16 +0200

    Page a propos

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,5 +1,53 @@
 function About() {
-  return <h1>About</h1>;
+  return (
+    <section>
+      <h1>À propos</h1>
+
+      <p>
+        Je suis développeur front-end junior et je développe des interfaces
+        web modernes, responsives et accessibles.
+      </p>
+
+      <p>
+        Depuis longtemps attiré par l’informatique et le développement web,
+        je ne m’étais pourtant pas orienté dans cette voie lors de mes études.
+        Après une première expérience professionnelle dans un autre domaine,
+        j’ai décidé de me reconvertir afin d’en faire mon métier.
+      </p>
+
+      <p>
+        Ma formation de développeur web chez OpenClassrooms m’a permis
+        d’acquérir progressivement les bases du développement front-end,
+        de HTML et CSS jusqu’à JavaScript et React. J’ai également découvert
+        le développement back-end avec Node.js, Express et MongoDB.
+      </p>
+
+      <p>
+        Ce que j’apprécie particulièrement dans le développement web, c’est
+        de continuer à découvrir et à approfondir mes connaissances au fil
+        des projets. Chacun d’eux m’a permis d’évoluer et d’élargir ma
+        compréhension d’une application web : de mon premier projet réalisé
+        simplement en HTML et CSS jusqu’à la découverte de React et du
+        développement back-end.
+      </p>
+
+      <p>
+        J’accorde une place importante à l’organisation de mon travail,
+        notamment à travers les principes de la méthodologie Agile. Je porte
+        également une attention particulière à la veille technologique, qui
+        me permet de suivre l’évolution rapide des technologies et des outils
+        du développement web et de continuer à faire évoluer mes pratiques.
+      </p>
+
+      <p>
+        Mon objectif est aujourd’hui de débuter ma carrière en tant que
+        développeur front-end junior, afin de mettre en pratique les
+        compétences acquises durant ma formation et de continuer à progresser
+        au contact de projets professionnels. Je souhaite également continuer
+        à élargir mes compétences, notamment en développement back-end.
+      </p>
+    </section>
+  );
 }
 
 export default About;
