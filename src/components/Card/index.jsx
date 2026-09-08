5d79e8211cf7d1d90f02258e8484000cc2d66f3f
Paul5123 <paul.guillon17@orange.fr>
2026-09-08 13:48:00 +0200

    Changement des outils utiliser par des images

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
 import "./Card.scss";
 
-function Card({ project }) {
-  const { id, image, imageAlt, title, technologies } = project;
+function Card({ project, technologies }) {
+  const { id, image, imageAlt, title } = project;
 
   return (
     <Link className="card" to={`/projects/${id}`}>
@@ -11,9 +11,14 @@ function Card({ project }) {
       <div className="card__content">
         <h3>{title}</h3>
 
-        <ul className="card__technologies">
-          {technologies.slice(0, 3).map((technology) => (
-            <li key={technology}>{technology}</li>
+        <ul
+          className="card__technologies"
+          aria-label={`Technologies utilisées pour ${title}`}
+        >
+          {technologies.map((technology) => (
+            <li key={technology.name}>
+              <img src={technology.logo} alt={technology.name} />
+            </li>
           ))}
         </ul>
       </div>
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -2,6 +2,34 @@ import Card from "../Card";
 import projects from "../../data/projects.json";
 import "./Projects.scss";
 
+const projectTechnologies = {
+  booki: [
+    { name: "HTML5", logo: "/icon/logo_html5.webp" },
+    { name: "CSS3", logo: "/icon/logo-css3.webp" },
+    { name: "Git", logo: "/icon/git-logo.webp" },
+  ],
+  "sophie-bluel": [
+    { name: "HTML5", logo: "/icon/logo_html5.webp" },
+    { name: "CSS3", logo: "/icon/logo-css3.webp" },
+    { name: "JavaScript", logo: "/icon/logo-javascript.webp" },
+  ],
+  "nina-carducci": [
+    { name: "HTML5", logo: "/icon/logo_html5.webp" },
+    { name: "CSS3", logo: "/icon/logo-css3.webp" },
+    { name: "JavaScript", logo: "/icon/logo-javascript.webp" },
+  ],
+  kasa: [
+    { name: "React", logo: "/icon/react-logo.webp" },
+    { name: "React Router", logo: "/icon/react-router-logo .webp" },
+    { name: "Sass", logo: "/icon/sass-logo.webp" },
+  ],
+  "mon-vieux-grimoire": [
+    { name: "Node.js", logo: "/icon/nodejs-logo.webp" },
+    { name: "Express", logo: "/icon/express-logo.webp" },
+    { name: "MongoDB", logo: "/icon/mongodb-logo.webp" },
+  ],
+};
+
 function Projects() {
   return (
     <section className="projects" id="projects">
@@ -13,7 +41,11 @@ function Projects() {
 
       <div className="projects__grid">
         {projects.map((project) => (
-          <Card key={project.id} project={project} />
+          <Card
+            key={project.id}
+            project={project}
+            technologies={projectTechnologies[project.id]}
+          />
         ))}
       </div>
     </section>
